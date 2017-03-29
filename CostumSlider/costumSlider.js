a040dae35b399c595d5608cb00346875db2e5452
张琳 <zhanglin@zhanglindeMacBook-Pro.local>
2017-03-29 17:39:46 +0800

    -自定义滑块效果，优化了内部代码逻辑

--- CostumSlider/costumSlider.js
+++ CostumSlider/costumSlider.js
@@ -7,37 +7,36 @@ import React, {
     PropTypes
 } from 'react';
 
-import {View, Image, Text,StyleSheet,TouchableOpacity,Dimensions,PanResponder,ART} from 'react-native';
-
+import {View, Image, Text,StyleSheet,TouchableOpacity,Dimensions,PanResponder} from 'react-native';
 var deviceWidth = Dimensions.get('window').width;
 
-const {Surface, Shape, Path} = ART;
 const Margin = 10;//间距
-const ThumbWidth = 60;//滑块内容宽度
-const LineWidth = deviceWidth - Margin*2 - ThumbWidth; //滑块横线宽度
 const DegreeWidth = 16; //刻度尺下面显示的文字的宽度
-const ArrowWidth = 6; //小箭头宽度
-const ArrowHeight =4; //小箭头高度
+
+var imageWidth = 0; //图片宽度
+var imageHeight = 0; //图片高度
+var lineWidth = 0; //滑块横线宽度
+var icon; //滑块显示的图片
 
 class CostumSlider extends React.Component {
 
     static propTypes = {
-        min: PropTypes.number.isRequired,
-        max: PropTypes.number.isRequired,
-        sep: PropTypes.number,
-        icon: PropTypes.string,
-        unit: PropTypes.string,
-        title: PropTypes.string,
+        min: PropTypes.number.isRequired, //最小值
+        max: PropTypes.number.isRequired, //最大值
+        sep: PropTypes.number, //歩长
+        type: PropTypes.string, //类型 'baby' ||  'money'
+        unit: PropTypes.string, //显示单位
+        title: PropTypes.string, //提示语
     }
 
 
     static get defaultProps() {
         return {
             min: 1,
-            max: 100,
-            sep: 1,
-            icon: require('../image/1234.png'),
-            unit: '岁',
+            max: 34,
+            sep: 3,
+            type: 'baby',
+            unit: '周',
             title: '请选择宝宝年龄',
         }
     }
@@ -49,6 +48,7 @@ class CostumSlider extends React.Component {
             value:this.props.min,//默认当前值
         };
 
+        this._initConfig = this._initConfig.bind(this);
 
     }
 
@@ -66,19 +66,20 @@ class CostumSlider extends React.Component {
                 let newValue = this.props.min;//滑块上方显示的值
 
 
+                console.log("aaaaa"+lineWidth);
                 if(newLeft<0){ //如果滑块已经滑到最左边  则不能继续向左滑动
                     newLeft = 0;
                     newValue = this.props.min;
-                }else if(newLeft>LineWidth){  //如果滑块已经滑到最右边，则不能继续向右滑动
-                    newLeft = LineWidth ;
+                }else if(newLeft>lineWidth){  //如果滑块已经滑到最右边，则不能继续向右滑动
+                    newLeft = lineWidth ;
                     newValue = this.props.max;
                 }else {
-                    if(newLeft%(LineWidth/space)>=(LineWidth/space)*0.8){  //如果当前滑块快接近到下一个刻度  则直接移动到下一个刻度
-                        newValue = this.props.min + Math.ceil(newLeft/(LineWidth/space)*this.props.sep);
-                        newLeft = (LineWidth/space) * ((newValue-this.props.min)/this.props.sep);
-                    }else if(newLeft%(LineWidth/space)<=(LineWidth/space)*0.2){  //如果当前滑块快接近到上一个刻度  则直接移动到上一个刻度
-                        newValue = this.props.min + Math.floor(newLeft/(LineWidth/space)*this.props.sep);
-                        newLeft = (LineWidth/space) * ((newValue-this.props.min)/this.props.sep);
+                    if(newLeft%(lineWidth/space)>=(lineWidth/space)*0.8){  //如果当前滑块快接近到下一个刻度  则直接移动到下一个刻度
+                        newValue = this.props.min + Math.ceil(newLeft/(lineWidth/space)*this.props.sep);
+                        newLeft = (lineWidth/space) * ((newValue-this.props.min)/this.props.sep);
+                    }else if(newLeft%(lineWidth/space)<=(lineWidth/space)*0.2){  //如果当前滑块快接近到上一个刻度  则直接移动到上一个刻度
+                        newValue = this.props.min + Math.floor(newLeft/(lineWidth/space)*this.props.sep);
+                        newLeft = (lineWidth/space) * ((newValue-this.props.min)/this.props.sep);
                     }else {return;}  //如果滑动处在两个刻度20%~80%之间  则不移动当前滑块
                 }
 
@@ -91,35 +92,44 @@ class CostumSlider extends React.Component {
         })
     }
 
+
+    ////////初始化配置/////////
+    _initConfig(){
+
+        if(this.props.type == 'baby'){
+            icon = require('../image/baby.png'),
+            imageWidth=60; //图片宽度
+            imageHeight=35; //图片高度
+            lineWidth=deviceWidth - Margin*2 - 60;//滑块宽度
+        }else if(this.props.type == 'money'){
+            icon = require('../image/money.png'),
+            imageWidth=76; //图片宽度
+            imageHeight=35; //图片高度
+            lineWidth=deviceWidth - Margin*2 - 76;//滑块宽度
+        }
+    }
+
+
+
     render() {
 
-        const path = new Path()
-            .moveTo(0,0)
-            .lineTo(ArrowWidth,0)
-            .lineTo(ArrowWidth/2,ArrowHeight)
-            .close();
+        this._initConfig();
 
         return (
             <View style={[styles.container,this.props.style]}>
                 {/* 显示提示词  例如  请选择宝宝年龄 */}
                 <Text style={styles.title}>{this.props.title}</Text>
-                {/* 显示滑动头部试图  左侧显示小图标  右侧显示当前值  值带单位  例如 '3岁' */}
-                <View style={[styles.thumbView,{left:this.state.left}]}  {...this._panResponder.panHandlers}>
-                    <Image style={styles.image} source={this.props.icon}/>
+                {/* 显示滑动头部视图   背景图+当前值 值带单位  例如 '3岁' */}
+                <Image  source={icon} style={[styles.thumbView,{left:this.state.left}]}  {...this._panResponder.panHandlers}>
                     <Text style={styles.content}>{this.state.value}{this.props.unit}</Text>
-                </View>
-                <View style={{marginBottom:10,left:this.state.left+(ThumbWidth-ArrowWidth)/2}}>
-                    <Surface width={ArrowWidth} height={ArrowHeight}>
-                        <Shape d={path}  fill="#fb4b88"  />
-                    </Surface>
-                </View>
+                </Image>
                 {/* 显示整个滑块条长度 */}
-                <View style={styles.sliderView}>
+                <View style={[styles.sliderView,{marginLeft:imageWidth/2,marginRight:imageWidth/2,}]}>
                     {/* 显示已滑动的长度 */}
                     <View style={[styles.sliderChildView,{width:this.state.left}]}></View>
                 </View>
                 {/* 显示刻度尺 */}
-                <View style={styles.degreeView}>
+                <View style={[styles.degreeView,{marginLeft:(imageWidth-DegreeWidth)/2, marginRight:(imageWidth-DegreeWidth)/2,}]}>
                     {this._addDegree()}
                 </View>
             </View>
@@ -152,7 +162,6 @@ module.exports = CostumSlider;
 
 const styles = StyleSheet.create({
     container:{
-        flex:1,
         padding:Margin,
         backgroundColor: 'white',
     },
@@ -163,33 +172,24 @@ const styles = StyleSheet.create({
         textAlign:'center',
     },
     thumbView:{
-        flexDirection: 'row',
-        alignItems: 'center',
         marginTop:15,
-        padding:5,
-        width:ThumbWidth,
-        height:35,
-        backgroundColor:'#fb4b88',
-        borderRadius:35/2.0,
-        overflow:'hidden',
-    },
-    image: {
-        resizeMode:'cover',
-        width:25,
-        height:25,
-        marginRight:3,
+        marginBottom:10,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     content: {
+        left:10,
+        marginBottom:3,
         fontSize:12,
         color:'white',
+        backgroundColor:'transparent',
     },
     sliderView:{
         height:6,
         borderRadius:3,
         overflow:'hidden',
         backgroundColor:'#e0e0e0',
-        marginLeft:ThumbWidth/2,
-        marginRight:ThumbWidth/2,
+
     },
     sliderChildView:{
         flex:1,
@@ -199,8 +199,6 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         marginTop:10,
-        marginLeft:(ThumbWidth-DegreeWidth)/2,
-        marginRight:(ThumbWidth-DegreeWidth)/2,
         marginBottom:5,
     },
     degreeText:{
--- index.ios.js
+++ index.ios.js
@@ -18,7 +18,8 @@ export default class CostumSliderProject extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <CostumSlider style={{marginTop:10}} min={1} max={34} sep={3} title={'请选择宝宝年龄'} unit={'周'} />
+        <CostumSlider style={{marginTop:60}} min={10} max={20} sep={2} title={'请选择心里价位'} unit={'元/月'} type={'money'} />
+        <CostumSlider style={{marginTop:10}} min={1} max={34} sep={3} title={'请选择宝宝年龄'} unit={'周'} type={'baby'} />
       </View>
     );
   }
